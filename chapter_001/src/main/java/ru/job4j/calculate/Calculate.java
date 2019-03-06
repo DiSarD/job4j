fd1ad0d6f6223e565784355e4c6fcffe7bebcd62
DiSarD <disard01@gmail.com>
2019-03-06 17:38:00 +0300

    1.3. JavaDoc. Code convention[#115446]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Класс для вывода текста на экран "Hello World"
+ * @author a.mkakrov
+ * @since 06.03.2018 
+ * @version 1
+ */
 public class Calculate{
 	
+/**
+ * Mian.
+ * @param arg - arg.
+ */	
 	public static void main (String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author a.makarov
+ * @version 1
+ * @since 06.03.2019
+ */
+package ru.job4j.calculate;
