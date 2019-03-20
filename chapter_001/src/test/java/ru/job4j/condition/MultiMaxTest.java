3dd14ecb60b26e0e3fa2d54b0d18f61b61e372f9
DiSarD <disard01@gmail.com>
2019-03-20 17:05:28 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#115417]

--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -11,14 +11,12 @@ public class MultiMaxTest {
         int result = check.max(1, 4, 2);
         assertThat(result, is(4));
     }
-
     @Test
     public void whenFirstMax() {
         MultiMax check = new MultiMax();
         int result = check.max(10, 4, 2);
         assertThat(result, is(10));
     }
-
     @Test
     public void whenThirdMax() {
         MultiMax check = new MultiMax();
